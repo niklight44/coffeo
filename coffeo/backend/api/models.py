0d07f09a178f47b73d56ff4e76e04672b831158a
Nik <russianik44@gmail.com>
2024-06-24 13:42:06 +0300

    added base models

--- coffeo/backend/api/models.py
+++ coffeo/backend/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class CoffeeBeans(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    price = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Accessories(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    price = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Apparel(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    price = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Apparel(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    price = models.FloatField(null=True)
+
+
+    def __str__(self):
+        return self.name
