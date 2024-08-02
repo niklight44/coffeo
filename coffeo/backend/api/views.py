1d66fdf20099660329292327eae3363627678905
Nik <russianik44@gmail.com>
2024-08-02 13:29:16 +0300

    UPDATE: optimize imports in api.views

--- coffeo/backend/api/views.py
+++ coffeo/backend/api/views.py
@@ -1,15 +1,15 @@
 from django.contrib.auth import login, logout
-from django.shortcuts import render
-from rest_framework import generics, status, permissions
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_GET
+from rest_framework import status, permissions
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Product
-from .serializers import ProductSerializer, UserRegisterSerializer, UserSerializer, UserLoginSerializer
-from django.http import JsonResponse
+from .serializers import ProductSerializer, UserRegisterSerializer, UserLoginSerializer
 
 
 @require_GET
