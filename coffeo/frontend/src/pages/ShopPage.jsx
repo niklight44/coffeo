639d946856bd7040e4c7a08630ce54854f6dfcee
Nik <russianik44@gmail.com>
2024-08-05 12:59:08 +0300

    ADD: useState to ShopPage, border to shopMenu and shopProducts

--- coffeo/frontend/src/pages/ShopPage.jsx
+++ coffeo/frontend/src/pages/ShopPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ShopMenu from "../components/ShopMenu.jsx";
 import ShopProducts from "../components/ShopProducts.jsx";
 import "../styles/ShopPage.css";
