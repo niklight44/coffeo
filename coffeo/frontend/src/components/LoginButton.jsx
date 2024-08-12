d152fd53539a649a194aa9332ac3e6cacab08449
Nik <russianik44@gmail.com>
2024-08-12 13:16:51 +0300

    FIX: displaying submenu when hovering on it

--- coffeo/frontend/src/components/LoginButton.jsx
+++ coffeo/frontend/src/components/LoginButton.jsx
@@ -6,7 +6,14 @@ const LoginButton = ({user}) => {
 
     return(
         <>
-            {user ? (<span className="auth-button">Hello, {user}</span>) : (
+            {user ? (
+                <>
+                    <span className="auth-button">Hello, {user}</span>
+                    <div className="submenu">
+                        <Link to="/order">Your Orders</Link>
+                    </div>
+                </>
+            ) : (
                     <Link to="/auth" className="auth-button">Log in/Sign up</Link>
             )}
         </>
